b79a0b278cf2d5ea24a4b1bfb517d5dce1e182c2
Kostia Dombrovsky <kdombrovsky@gmail.com>
2013-09-11 17:19:50 +0300

    Initial Commit

---
+++ AndroidLocalizationUtility/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AndroidLocalizationUtility
+//
+//  Created by Kostia Dombrovsky on 9/11/13.
+//  Copyright (c) 2013 Kostia Dombrovsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
